b88e881455fbbd8af597b2d39cb446bb446b897d
Qoder-Undefined <aliyudeeny3@gmail.com>
2026-06-12 12:42:48 +0000

    Created the posgres database

---
+++ database/database.module.ts
@@ -0,0 +1,20 @@
+// src/database/database.module.ts
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+  ],
+})
+export class DatabaseModule {}
